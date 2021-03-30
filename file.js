1140254db31570fce5d4a0f21835c4a7f1ae408b
Joseph Grainger <grrainger7@gmail.com>
2021-03-30 13:50:43 +0200

    finished

---
+++ file.js
@@ -0,0 +1,34 @@
+const snackServings = 5;
+console.log(snackServings);
+const guests = 2;
+console.log(guests);
+
+console.log(snackServings + guests);
+
+console.log(snackServings - guests); 
+console.log(guests - snackServings);
+
+console.log(snackServings * guests);
+
+console.log(snackServings / guests);
+
+const drinks = 10;
+
+const resultOne = snackServings * guests / drinks;
+console.log(resultOne);
+
+let monkeys = 15;
+let bananas = 9;
+console.log(monkeys / bananas);
+
+const monkeyfights = 20;
+let resultTwo = (monkeys + bananas) * monkeyfights;
+console.log(monkeyfights);
+
+console.log(monkeys++);
+console.log(bananas--);
+
+let happiness = bananas - monkeys;
+console.log(happiness + monkeyfights);
+
+console.log(resultOne % resultTwo);
